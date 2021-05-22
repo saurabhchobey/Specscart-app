983db080e81be2933c506c0d6c57dc86d122ed7a
saurabhchobey <52131942+saurabhchobey@users.noreply.github.com>
2021-05-22 16:57:11 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Specscart-app
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/components/pages/Services.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../../App.css';
+import Banner from '../Banner';
+import Cards from '../Cards';
+import Footer from '../Footer';
+
+import { Button } from '../Button';
+import '../HeroSection.css';
+
+
+export default function Services() {
+  const image='https://farm2.staticflickr.com/1638/26145024230_06acd55d1b_b.jpg'
+  return <>
+  {/* <h1 className='services'>SERVICES</h1> */}
+  <Banner className='Banner'>
+  
+  
+
+  </Banner>
+  <Cards/>
+  <Footer/>
+  </>;
+}
